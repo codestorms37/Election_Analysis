32a96fbdf0f89be9e796ad61c68b7d783c443f3b
codestorms37 <46337583+codestorms37@users.noreply.github.com>
2021-07-01 01:19:23 -0500

    Added PyPoll and Resources
    election_results.csv

---
+++ PyPoll.py
@@ -0,0 +1,13 @@
+import csv
+
+# dir() : will return all the functions available in the csv module.
+print(dir(csv))
+
+
+# RESULT
+
+# 1. Total number of votes cast
+# 2. A complete list of candidates who received votes
+# 3. Total number of votes each candidate received
+# 4. Percentage of votes each candidate won
+# 5. The winner of the election based on popular vote
