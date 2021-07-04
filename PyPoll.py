06cc4ddcc60b0803ff5f5bd7ee5d13f7ea28a580
codestorms37 <46337583+codestorms37@users.noreply.github.com>
2021-07-04 15:54:27 -0500

    Readme Update
    Simplified tables, and smaller results image

--- PyPoll.py
+++ PyPoll.py
@@ -144,3 +144,16 @@ with open(file_to_save_path, "w", newline="" ) as txt_file_pointer:
 
     # Save the winning candidate's name to the text file.
     txt_file_pointer.write(winning_candidate_summary)
+
+
+# The election commission has requested some additional data to complete the audit:
+
+## The voter turnout for each county
+### print
+### save
+## The percentage of votes from each county out of the total count
+### print
+### save
+## The county with the highest turnout
+### print
+### save
